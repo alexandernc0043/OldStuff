ee0d00b85fe5fa32038f426e50ed0896c933ec7d
alexnc <professornc1205@gmail.com>
2014-12-12 19:48:01 -0500

    awesome code

---
+++ do this.py
@@ -0,0 +1,15 @@
+print('Hello what is your name')
+name = input('>')
+
+
+print('WellHello',name,'.')
+
+print('What is your age')
+age = input('>')
+print('Ok you are',age,'.')
+print('What is your School')
+School = input('>')
+print('',School,' well that school sounds nice')
+
+
+
