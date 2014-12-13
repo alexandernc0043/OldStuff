63ff37122db83e126a4b71233777cc6671ff3637
alexnc <professornc1205@gmail.com>
2014-12-13 08:31:10 -0500

    Edited do this.py

--- do this.py
+++ do this.py
@@ -2,7 +2,7 @@ print('Hello what is your name')
 name = input('>')
 
 
-print('WellHello',name,'.')
+print('Well Hello',name,'.')
 
 print('What is your age')
 age = input('>')
@@ -10,6 +10,12 @@ print('Ok you are',age,'.')
 print('What is your School')
 school = input('>')
 print('',school,' well that school sounds nice')
+
+print('Whats your Favorite game?')
+game = input('>')
+print('ok',game,' sounds fun')
+
+
 
 
 
