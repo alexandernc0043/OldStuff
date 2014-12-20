fb1a2bfe21bbc8e2c0cd2e0d0d8b1ae6add0b3b9
alexnc <professornc1205@gmail.com>
2014-12-20 17:21:48 -0500

    Code

--- do this.py
+++ do this.py
@@ -14,29 +14,13 @@ print('',school,' well that school sounds nice')
 print('Whats your Favorite game?')
 game = input('>')
 print('ok',game,' sounds fun')
-<<<<<<< HEAD
-while fnaf not ('yes','no'):
-=======
-
->>>>>>> origin/master
-
+fnaf = input('> ')
+while fnaf is not ('yes','no'):
     print('do you like FNAF?')
-    fnaf = input('>')
     if fnaf == 'yes':
-<<<<<<< HEAD
         print('I like FNAF also')
     if fnaf == 'no':
         print('man you dont like FNAF')
     if fnaf == 'maybe':
-    
-=======
-print('I like FNAF also')
-    if fnaf == 'no':
-    print('man you dont like FNAF')
-print('last question:what rocks more pika Girl a.k.a Michyela or Gabe miller(put Pika girl for michyela)')
-answer = input('>')
-if answer == 'pika Girl':
-    print('Incorrect')
-if answer == 'Gabe miller':
-    print('Correct')
->>>>>>> origin/master
+        print('tell the truth')
+
