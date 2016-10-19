496093e69adbab1f801c322afd37a37c39af86d1
Alex Prechtel <professornc1205@gmail.com>
2016-10-19 17:03:53 -0400

    Update dodge.js

--- games/dodge.js
+++ games/dodge.js
@@ -4,9 +4,18 @@ class Boot {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
   }
+  create() {
+    this.state.start("Load")
+  }
 }
-
-
+class Load {
+  preLoad() {
+    console.log("Loading...")
+  }
+  create() {
+    console.log("Loaded");
+  }
+}
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
 game.state.start("Boot");
