cf78e36b128ef98594437bda20f51ebe3f58901c
alexnc <professornc1205@gmail.com>
2016-12-07 17:17:13 -0500

    save

--- dodgegame/dodge.js
+++ dodgegame/dodge.js
@@ -17,7 +17,8 @@ var C = {
       "frames": 4,
       "fps": 4,
       "startx": 160,
-      "starty": 500
+      "starty": 500,
+      "speed": 5
 },
     "d": {
       "file": "assets/dodgeme.png",
@@ -74,9 +75,21 @@ class Play {
     this.dodge.scale.set(1);
     this.dodge.animations.add("anim");
     this.dodge.animations.play("anim",C.d.fps,true);
+ 
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
   update() {
     console.log("Play.update() called.");
+    if (this.cursors.left.isDown) {
+      this.player.x -= C.p.speed;
+    }
+
+  if (this.cursors.right.isDown) {
+    this.player.x += C.p.speed;
+    }
+  }
+  render() {
+    game.debug.text("x: " + this.doge.x + ", y: " + this.doge.y, 4, 16);
   }
 }
 
